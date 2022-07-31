1798dfc71806785fbabdea3d5333b2fc42fafa0a
Aoshi Fujioka <blue20will@gmail.com>
2022-07-31 14:01:01 +0900

    feat: update matches

--- yakuman/pages/matches.vue
+++ yakuman/pages/matches.vue
@@ -1,17 +1,21 @@
 <template>
-    <main>
-        <a-page-header title="戦一覧" :breadcrumb="{ props: { routes } }" />
-        <a-spin size="large" tip="loading..." :spinning="loading">
-            <a-table
-                :data-source="matchesWithJansou"
-                :columns="columns"
-            >
-                <span slot="nameRenderer" slot-scope="name, record">
-                    <NuxtLink :to="`/match/${record.id}`">{{ name }}</NuxtLink>
-                </span>
-            </a-table>
-        </a-spin>
-    </main>
+    <v-container>
+        <h1>戦一覧</h1>
+        <v-spacer></v-spacer>
+        <v-breadcrumbs :items="items" large></v-breadcrumbs>
+        <v-spacer></v-spacer>
+
+        <v-data-table
+            item-key="id"
+            :headers="headers"
+            :items="matchesWithJansou"
+            class="row-pointer"
+            :loading="loading"
+            loading-text="よみこみちゅう..."
+            @click:row="onClickRow"
+        >
+        </v-data-table>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -26,30 +30,17 @@ type MatchWithJansou = Match & {
 export default Vue.extend({
     name: 'MahjongMatch',
     data: () => ({
-        routes: [
-            { path: '/', breadcrumbName: 'Home' },
-            { path: '/matches', breadcrumbName: 'matches' },
+        items: [
+            { text: 'Top', disabled: false, href: '/' },
+            { text: '戦一覧', disabled: true, href: '/matches' },
         ],
         matches: [] as Match[],
         jansous: [] as Jansou[],
         matchesWithJansou: [] as MatchWithJansou[],
-        columns: [
-            {
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name',
-                scopedSlots: { customRender: 'nameRenderer' },
-            },
-            {
-                title: 'Jansou',
-                dataIndex: 'jansouName',
-                key: 'jansouName',
-            },
-            {
-                title: 'Held at',
-                dataIndex: 'created_at',
-                key: 'created_at',
-            },
+        headers: [
+            { text: '名前', value: 'name' },
+            { text: '雀荘', value: 'jansouName' },
+            { text: '開催日', value: 'created_at' },
         ],
         loading: true,
     }),
@@ -77,6 +68,15 @@ export default Vue.extend({
             })
             this.matchesWithJansou = matchesWithJansou
         },
+        onClickRow(_: any, { item }) {
+            this.$router.push(`/match/${item.id}`)
+        },
     },
 })
 </script>
+
+<style scoped>
+.row-pointer >>> tbody tr :hover {
+    cursor: pointer;
+}
+</style>
